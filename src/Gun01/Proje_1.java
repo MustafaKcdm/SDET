6ebb0e96a161cbd94c12dbabaaf3cbf5778738d8
MustafaKcdm <mmustafakocaadam@gmail.com>
2023-02-02 19:42:13 +0300

    c2

--- src/Gun01/Proje_1.java
+++ src/Gun01/Proje_1.java
@@ -1,2 +1,12 @@
-package Gun01;public class Proje_1 {
+package Gun01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Proje_1 {
+    public static void main(String[] args) {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.techno.study/");
+
+    }
 }
